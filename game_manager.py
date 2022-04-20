07de5f0bfb6fc93d11f21ad30aa9787727d62d19
Ziang Tong <97473223+DannyTDS@users.noreply.github.com>
2022-04-20 12:39:19 -0400

    Update: two players, competition mode

--- game_manager.py
+++ game_manager.py
@@ -32,15 +32,27 @@ def load_prefabs(prefab_paths):
 	head = pygame.transform.scale(head, (20, 20))
 	body = pygame.image.load(prefab_paths["body"])
 	body = pygame.transform.scale(body, (20, 20))
+	playerA_head = pygame.image.load(prefab_paths["playerA_head"])
+	playerA_head = pygame.transform.scale(playerA_head, (20, 20))
+	playerA_body = pygame.image.load(prefab_paths["playerA_body"])
+	playerA_body = pygame.transform.scale(playerA_body, (20, 20))
+	playerB_head = pygame.image.load(prefab_paths["playerB_head"])
+	playerB_head = pygame.transform.scale(playerB_head, (20, 20))
+	playerB_body = pygame.image.load(prefab_paths["playerB_body"])
+	playerB_body = pygame.transform.scale(playerB_body, (20, 20))
 	gd_fruit = pygame.image.load(prefab_paths["gd_fruit"])
 	gd_fruit = pygame.transform.scale(gd_fruit, (20, 20))
 	bd_fruit = pygame.image.load(prefab_paths["bd_fruit"])
 	bd_fruit = pygame.transform.scale(bd_fruit, (20, 20))
 	prefabs = {
-		"head"		: head,
-		"body" 		: body,
-		"gd_fruit"	: gd_fruit,
-		"bd_fruit"	: bd_fruit,
+		"head"			: head,
+		"body" 			: body,
+		"playerA_head"	: playerA_head,
+		"playerA_body"	: playerA_body,
+		"playerB_head"	: playerB_head,
+		"playerB_body"	: playerB_body,
+		"gd_fruit"		: gd_fruit,
+		"bd_fruit"		: bd_fruit,
 	}
 	return prefabs
 
@@ -122,12 +134,49 @@ def update(screen, score, the_snake, active_fruits, prefabs, bg_color=BLACK):
 	pygame.display.update()
 
 
-def update_occupancy(the_snake, active_fruits):
+def update_two_players(screen, player_A, player_B, active_fruits, prefabs, bg_color=BLACK):
+	screen.fill(bg_color)
+	
+	for i, node in enumerate(player_A.get_body()):
+		if i == 0:
+			# head
+			head_rect = prefabs["playerA_head"].get_rect(center=node.get_pos())
+			screen.blit(prefabs["playerA_head"], head_rect)
+		else:
+			# body
+			body_rect = prefabs["playerA_body"].get_rect(center=node.get_pos())
+			screen.blit(prefabs["playerA_body"], body_rect)
+	
+	for i, node in enumerate(player_B.get_body()):
+		if i == 0:
+			# head
+			head_rect = prefabs["playerB_head"].get_rect(center=node.get_pos())
+			screen.blit(prefabs["playerB_head"], head_rect)
+		else:
+			# body
+			body_rect = prefabs["playerB_body"].get_rect(center=node.get_pos())
+			screen.blit(prefabs["playerB_body"], body_rect)
+	
+	for fruit in active_fruits:
+		if(fruit.get_type() == "gd"):
+			fruit_rect = prefabs["gd_fruit"].get_rect(center=fruit.get_pos())
+			screen.blit(prefabs["gd_fruit"], fruit_rect)
+		elif(fruit.get_type() == "bd"):
+			fruit_rect = prefabs["bd_fruit"].get_rect(center=fruit.get_pos())
+			screen.blit(prefabs["bd_fruit"], fruit_rect)
+	
+	pygame.display.update()	
+
+
+def update_occupancy(the_snake, active_fruits, another_snake=None):
 	occupied = []
 	for node in the_snake.get_body():
 		occupied.append(node.get_pos())
 	for fruit in active_fruits:
 		occupied.append(fruit.get_pos())
+	if another_snake:
+		for node in another_snake.get_body():
+			occupied.append(node.get_pos())
 	return occupied
 
 
@@ -169,6 +218,24 @@ def self_collision(the_snake):
 	return False
 
 
+# Collision logic for two players: one player crashes into another
+def collide_into(one_player, another_player):
+	head_pos = one_player.get_body()[0].get_pos()
+	
+	if head_pos[0] < 0 or head_pos[0] > width:
+		return True
+	if head_pos[1] < 0 or head_pos[1] > height:
+		return True
+		
+	# check collision with body
+	for (i, node) in enumerate(another_player.get_body()):
+		if node.get_pos() == head_pos:
+			return True
+	
+	# no collision detected
+	return False
+
+
 # Fruits decay overtime
 def decay_fruits(active_fruits):
 	for fruit in active_fruits:
@@ -179,7 +246,7 @@ def decay_fruits(active_fruits):
 
 
 # Game over animation controller
-def game_over(screen, the_snake, prefabs, mode, bg_color=BLACK):
+def classic_game_over(screen, the_snake, score, prefabs, bg_color=BLACK):
 	screen.fill(bg_color)
 	
 	# Gradually pops back from the snake
@@ -198,22 +265,71 @@ def game_over(screen, the_snake, prefabs, mode, bg_color=BLACK):
 				screen.blit(prefabs["body"], body_rect)
 	
 		pygame.display.update()
-		pygame.time.wait(25)
+		pygame.time.wait(50)
 
-	if mode[0] == "classic":
-		message_to_screen(screen, "Game Over",
-			RED, (width/2, height/2 - 10), "large", "center")
-		score = mode[1]
-		message_to_screen(screen, f"You scored: {score}",
-			WHITE, (width/2, height/2 + 20), "small", "center")
-		message_to_screen(screen, "Press any key to quit",
-			WHITE, (width/2, height/2 + 40), "small", "center")
+	message_to_screen(screen, "Game Over",
+		RED, (width/2, height/2 - 10), "large", "center")
+	message_to_screen(screen, f"You scored: {score}",
+		WHITE, (width/2, height/2 + 20), "small", "center")
+	message_to_screen(screen, "Press any key to quit",
+		WHITE, (width/2, height/2 + 40), "small", "center")
 
+	pygame.display.update()
+	
+	pygame.time.wait(1000)
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				sys.exit()
+
+
+def combat_game_over(screen, player_A, player_B, prefabs, winner, bg_color=BLACK):
+	screen.fill(bg_color)
+	
+	if winner == "A":
+		loser = player_B
+	elif winner == "B":
+		loser = player_A
+		
+	# Gradually pops back from the snake
+	while(loser.is_alive()):
+		loser.rm_body()
+		screen.fill(bg_color)	
+		
+		for i, node in enumerate(player_A.get_body()):
+			if i == 0:
+				# head
+				head_rect = prefabs["playerA_head"].get_rect(center=node.get_pos())
+				screen.blit(prefabs["playerA_head"], head_rect)
+			else:
+				# body
+				body_rect = prefabs["playerA_body"].get_rect(center=node.get_pos())
+				screen.blit(prefabs["playerA_body"], body_rect)
+		
+		for i, node in enumerate(player_B.get_body()):
+			if i == 0:
+				# head
+				head_rect = prefabs["playerB_head"].get_rect(center=node.get_pos())
+				screen.blit(prefabs["playerB_head"], head_rect)
+			else:
+				# body
+				body_rect = prefabs["playerB_body"].get_rect(center=node.get_pos())
+				screen.blit(prefabs["playerB_body"], body_rect)
+	
 		pygame.display.update()
-		
-		pygame.time.wait(1000)
-		
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.KEYDOWN:
-					sys.exit()
+		pygame.time.wait(50)
+
+	message_to_screen(screen, f"Player {winner} Wins!",
+		RED, (width/2, height/2 - 10), "large", "center")
+	message_to_screen(screen, "Press any key to quit",
+		WHITE, (width/2, height/2 + 40), "small", "center")
+
+	pygame.display.update()
+	
+	pygame.time.wait(1000)
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				sys.exit()
--- snake_main.py
+++ snake_main.py
@@ -22,6 +22,10 @@ BLUE	= (0, 0, 255)
 prefab_paths = {
 	"head"		: "./Assets/sg_player_head.png",
 	"body" 		: "./Assets/sg_player_body.png",
+	"playerA_head"	: "./Assets/db_playerA_head.png",
+	"playerA_body"	: "./Assets/db_playerA_body.png",
+	"playerB_head"	: "./Assets/db_playerB_head.png",
+	"playerB_body"	: "./Assets/db_playerB_body.png",
 	"gd_fruit"	: "./Assets/gd_fruit.png",
 	"bd_fruit"	: "./Assets/bd_fruit.png",
 }
@@ -33,6 +37,15 @@ turning_keys = [
 	pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN,
 ]
 
+# for competition mode
+playerA_turning_keys = [
+	pygame.K_d, pygame.K_a, pygame.K_w, pygame.K_s,
+]
+
+playerB_turning_keys = [
+	pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN,
+]
+
 
 # GLOBALS
 width 		= 1200							# size of the window
@@ -40,7 +53,7 @@ height 		= 800
 speed 		= 20							# player speed
 direction 	= 'r'							# player direction
 bg_color 	= BLACK							# background color, in RGB
-frame_time 	= 50							# wait time between frames, in ms
+frame_time 	= 80							# wait time between frames, in ms
 fruit_interval = 1500						# wait time between generation of fruits, in ms
 	
 
@@ -49,14 +62,15 @@ def main():
 	screen 	= gm.setup((width, height))
 	prefabs = gm.load_prefabs(prefab_paths)
 	
-	start_classic(screen, prefabs)
-
+	# start_classic(screen, prefabs)
+	
+	start_combat(screen, prefabs)
 
 
 # Single player mode
 def start_classic(screen, prefabs):
 	# initialize player
-	the_snake = Snake((width/2, height/2))
+	the_snake = Snake((width/2, height/2), 'r')
 	for i in range(2):
 		the_snake.add_body()
 	
@@ -109,7 +123,7 @@ def start_classic(screen, prefabs):
 		
 		# check if game over
 		if (not the_snake.is_alive() or gm.self_collision(the_snake)):
-			gm.game_over(screen, the_snake, prefabs, ("classic", score))
+			gm.classic_game_over(screen, the_snake, score, prefabs)
 		
 		pygame.time.wait(frame_time)
 		
@@ -121,7 +135,90 @@ def start_classic(screen, prefabs):
 
 # Two players mode
 def start_combat(screen, prefabs):
-	pass
+	'''
+	special rules:
+	1. can cross the snake itself
+	2. try to surround the other snake
+	3. die when crash into the other snake
+	4. faster fruit generation
+	'''
+	player_A = Snake((40, 40), 'r')
+	player_B = Snake((width-40, height-40), 'l')
+	for i in range(2):
+		player_A.add_body()
+		player_B.add_body()
+
+# initialize screen color and score
+	fruit_interval = 500
+	fruit_cooldown = 0
+	active_fruits = []
+	occupied_slots = gm.update_occupancy(player_A, active_fruits, player_B)
+	screen.fill(bg_color)
+	pygame.display.update()
+	
+	while 1:
+		# event control
+		for event in pygame.event.get():
+			# quit game
+			if event.type == pygame.QUIT:
+				sys.exit()
+			# player turning
+			elif event.type == pygame.KEYDOWN:
+				if event.key in playerA_turning_keys:
+					if event.key == pygame.K_d:
+						tmp_A_dir = 'r'
+					elif event.key == pygame.K_a:
+						tmp_A_dir = 'l'
+					elif event.key == pygame.K_w:
+						tmp_A_dir = 'u'
+					elif event.key == pygame.K_s:
+						tmp_A_dir = 'd'
+					if tmp_A_dir in player_A.get_eligible_moves():
+						player_A.set_head_dir(tmp_A_dir)
+				elif event.key in playerB_turning_keys:
+					if event.key == pygame.K_RIGHT:
+						tmp_B_dir = 'r'
+					elif event.key == pygame.K_LEFT:
+						tmp_B_dir = 'l'
+					elif event.key == pygame.K_UP:
+						tmp_B_dir = 'u'
+					elif event.key == pygame.K_DOWN:
+						tmp_B_dir = 'd'
+					if tmp_B_dir in player_B.get_eligible_moves():
+						player_B.set_head_dir(tmp_B_dir)
+				elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+					sys.exit()
+				elif event.key == pygame.K_p or event.key == pygame.K_SPACE:
+					gm.pause(screen)
+
+		# character control
+		player_A.update_body(speed)
+		player_B.update_body(speed)
+		
+		# check if player ate fruit
+		gm.update_eat_fruit(player_A, active_fruits)
+		gm.update_eat_fruit(player_B, active_fruits)
+		
+		# check if new fruit needs to be generated
+		if(fruit_cooldown >= fruit_interval/frame_time):
+			active_fruits.append(Fruit(occupied_slots))
+			fruit_cooldown = 0
+
+		# update screen
+		gm.update_two_players(screen, player_A, player_B, active_fruits, prefabs, bg_color)
+		
+		# check if game over
+		if (not player_A.is_alive() or gm.collide_into(player_A, player_B)):
+			gm.combat_game_over(screen, player_A, player_B, prefabs, "B")
+		if (not player_B.is_alive() or gm.collide_into(player_B, player_A)):
+			gm.combat_game_over(screen, player_A, player_B, prefabs, "A")
+		
+		pygame.time.wait(frame_time)
+		
+		# increment fruit_cooldown and update occupied_slots
+		fruit_cooldown += 1
+		occupied_slots = gm.update_occupancy(player_A, active_fruits, player_B)
+		active_fruits = gm.decay_fruits(active_fruits)
 
 
 if __name__ == "__main__":
