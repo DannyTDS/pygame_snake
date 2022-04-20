2b499313862a46d0935a3eb1064561c81207854d
Ziang Tong <97473223+DannyTDS@users.noreply.github.com>
2022-04-19 22:13:16 -0400

    Update snake_main: single player

--- snake_main.py
+++ snake_main.py
@@ -6,8 +6,9 @@ to start the game.
 
 import sys, pygame
 from snake import Snake
+from fruit import Fruit
 import game_manager as gm
-# TODO: keep track of rects in Snake, instead of re-drawing every frame
+
 
 # color syntax sugars
 BLACK 	= (0, 0, 0)
@@ -19,10 +20,10 @@ BLUE	= (0, 0, 255)
 
 # prefab paths
 prefab_paths = {
-	"head"		: "./Assets/test_square.png",
-	"body" 		: "./Assets/test_square.png",
-	"gd_fruit"	: "./Assets/test_square.png",
-	"bd_fruit"	: "./Assets/test_square.png",
+	"head"		: "./Assets/sg_player_head.png",
+	"body" 		: "./Assets/sg_player_body.png",
+	"gd_fruit"	: "./Assets/gd_fruit.png",
+	"bd_fruit"	: "./Assets/bd_fruit.png",
 }
 
 
@@ -34,12 +35,13 @@ turning_keys = [
 
 
 # GLOBALS
-width 		= 800							# size of the window
+width 		= 1200							# size of the window
 height 		= 800
 speed 		= 20							# player speed
 direction 	= 'r'							# player direction
 bg_color 	= BLACK							# background color, in RGB
 frame_time 	= 50							# wait time between frames, in ms
+fruit_interval = 1500						# wait time between generation of fruits, in ms
 	
 
 def main():
@@ -47,17 +49,24 @@ def main():
 	screen 	= gm.setup((width, height))
 	prefabs = gm.load_prefabs(prefab_paths)
 	
-	# TODO: test player only. combine with Snake class
+	start_classic(screen, prefabs)
+
+
+
+# Single player mode
+def start_classic(screen, prefabs):
+	# initialize player
 	the_snake = Snake((width/2, height/2))
-	
-	# Testing only: added some body segments
-	for i in range(10):
+	for i in range(2):
 		the_snake.add_body()
 	
 	# initialize screen color and score
 	score = 0
+	fruit_cooldown = 0
+	active_fruits = []
+	occupied_slots = gm.update_occupancy(the_snake, active_fruits)
 	global direction
-	screen.fill(BLACK)
+	screen.fill(bg_color)
 	pygame.display.update()
 	
 	while 1:
@@ -79,17 +88,40 @@ def main():
 						direction = 'd'
 					if direction in the_snake.get_eligible_moves():
 						the_snake.set_head_dir(direction)
-				elif event.key == pygame.K_q:
+				elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
 					sys.exit()
-				elif event.key == pygame.K_p:
+				elif event.key == pygame.K_p or event.key == pygame.K_SPACE:
 					gm.pause(screen)
 
 		# character control
 		the_snake.update_body(speed)
 		
+		# check if player ate fruit
+		score += gm.update_eat_fruit(the_snake, active_fruits)
+		
+		# check if new fruit needs to be generated
+		if(fruit_cooldown >= fruit_interval/frame_time):
+			active_fruits.append(Fruit(occupied_slots))
+			fruit_cooldown = 0
+
 		# update screen
-		gm.update(screen, score, the_snake, prefabs, bg_color)
+		gm.update(screen, score, the_snake, active_fruits, prefabs, bg_color)
+		
+		# check if game over
+		if (not the_snake.is_alive() or gm.self_collision(the_snake)):
+			gm.game_over(screen, the_snake, prefabs, ("classic", score))
+		
 		pygame.time.wait(frame_time)
+		
+		# increment fruit_cooldown and update occupied_slots
+		fruit_cooldown += 1
+		occupied_slots = gm.update_occupancy(the_snake, active_fruits)
+		active_fruits = gm.decay_fruits(active_fruits)
+
+
+# Two players mode
+def start_combat(screen, prefabs):
+	pass
 
 
 if __name__ == "__main__":
